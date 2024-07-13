2d9f1bebb80f833a675cbf2085dae425d6dc03cf
mo6x <christmoses1999@gmail.com>
2024-07-13 20:02:15 +0100

    tested dev

--- BuyMtnDate_Frontend/src/hooks/useAxios.ts
+++ BuyMtnDate_Frontend/src/hooks/useAxios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8000/api"
+const baseUrl = import.meta.env.VITE_BASE_URL
 const getToken = () => {
    const token = localStorage.getItem('authToken');
 
